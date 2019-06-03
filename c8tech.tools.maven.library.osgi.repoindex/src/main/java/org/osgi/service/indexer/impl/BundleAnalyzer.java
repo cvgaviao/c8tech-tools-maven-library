8e3e3966fb97a29e9276c1e17f93e455b2bf08ba
Cristiano V. Gavião <cvgaviao@gmail.com>
2019-06-03 08:52:28 -0300

    upgrade headers

--- c8tech.tools.maven.library.osgi.repoindex/src/main/java/org/osgi/service/indexer/impl/BundleAnalyzer.java
+++ c8tech.tools.maven.library.osgi.repoindex/src/main/java/org/osgi/service/indexer/impl/BundleAnalyzer.java
@@ -1,6 +1,6 @@
 /**
  * ==========================================================================
- * Copyright © 2015-2018 OSGi Alliance, Cristiano Gavião.
+ * Copyright © 2015-2019 OSGi Alliance, Cristiano Gavião.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
