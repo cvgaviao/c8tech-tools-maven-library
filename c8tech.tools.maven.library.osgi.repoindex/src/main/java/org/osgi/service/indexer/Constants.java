8a472fc3085d7c4b48cd40edab5231f95f29191c
Cristiano V. Gavião <cvgaviao@gmail.com>
2019-06-06 23:16:48 -0300

    upgrade to new parent pom version
    changed license headers

--- c8tech.tools.maven.library.osgi.repoindex/src/main/java/org/osgi/service/indexer/Constants.java
+++ c8tech.tools.maven.library.osgi.repoindex/src/main/java/org/osgi/service/indexer/Constants.java
@@ -1,15 +1,13 @@
 /**
- * ==========================================================================
- * Copyright © 2015-2018 OSGi Alliance, Cristiano Gavião.
+ * ======================================================================
+ * Copyright © 2015-2019, OSGi Alliance, Cristiano V. Gavião.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- * Cristiano Gavião (cvgaviao@c8tech.com.br)- initial API and implementation
- * ==========================================================================
+ * =======================================================================
  */
 package org.osgi.service.indexer;
 
